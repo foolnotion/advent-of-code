94d2dffd5da0d77e6bba4af9913c1355074efc8a
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-10-18 11:42:39 +0200

    2017 day 1

--- source/2017/01/solution.cpp
+++ source/2017/01/solution.cpp
@@ -1,11 +1,7 @@
-#include <iterator>
 #include <aoc.hpp>
-#include <scn/detail/file.h>
 
 template<>
 auto advent<2017>::day01() const -> void {
-    fmt::print("2017 day 1\n");
-
     std::fstream f("./source/2017/01/input.txt");
     if (!f.is_open()) {
         fmt::print(stderr, "failed to open filename\n");
@@ -15,7 +11,6 @@ auto advent<2017>::day01() const -> void {
     {
         std::string line;
         std::getline(f, line);
-        fmt::print("{}\n", line);
         for (auto it = line.begin(); it < line.end(); ++it) {
             std::string_view sv{it, it+1};
             int d;
@@ -27,10 +22,18 @@ auto advent<2017>::day01() const -> void {
     auto const sz = std::ssize(u);
     u64 sum1{0};
     u64 sum2{0};
-    for (auto i = 0L, j = i+1, k = sz/2; i < sz; ++i, j = (++j % sz), k = (++k % sz)) {
+
+    i64 j{1};
+    i64 k{sz/2};
+
+    for (auto i = 0; i < sz; ++i) {
         sum1 += u[i] * (u[i] == u[j]);
-        sum2 += u[i] * (u[i] == u[k]); 
+        sum2 += u[i] * (u[i] == u[k]);
+
+        j = (j+1) % sz;
+        k = (k+1) % sz;
     }
+
     fmt::print("part 1: {}\n", sum1);
     fmt::print("part 2: {}\n", sum2);
 }
