3948eda92db114c11500dfa6e1b267d2f80d4078
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-10-25 17:09:38 +0200

    2015 day 9: formatting

--- source/2015/09/solution.cpp
+++ source/2015/09/solution.cpp
@@ -19,14 +19,14 @@ auto advent<2015>::day09() const -> void // NOLINT
         (void)scn::scan(s, "{} to {} = {}", a, b, d);
         auto ha = hash(a);
         auto hb = hash(b);
-        names.insert({ha, a});
-        names.insert({hb, b});
+        names.insert({ ha, a });
+        names.insert({ hb, b });
         dist[{ ha, hb }] = dist[{ hb, ha }] = d;
     }
 
     std::vector<u64> cities = lz::map(names, [](auto const& x) { return x.first; }).toVector();
 
-    i64 goal{};
+    i64 goal {};
     robin_hood::unordered_flat_set<u64> visited;
     auto search = [&]<typename F, typename G>(auto curr, auto depth, auto distance, F accept, G reject, auto&& search) {
         visited.insert(curr);
@@ -36,9 +36,11 @@ auto advent<2015>::day09() const -> void // NOLINT
             return;
         }
         for (auto next : lz::filter(cities, [&](auto x) { return !visited.contains(x); })) {
-            auto d = dist[{curr, next}];
-            if (reject(distance + d, goal)) { continue; }
-            search(next, depth+1, distance + d, accept, reject, search);
+            auto d = dist[{ curr, next }];
+            if (reject(distance + d, goal)) {
+                continue;
+            }
+            search(next, depth + 1, distance + d, accept, reject, search);
         }
         visited.erase(curr);
     };
