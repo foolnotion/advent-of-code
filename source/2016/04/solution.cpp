17651c314695c72fcb3a3f24a75ecc3e71db368c
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-11-17 00:18:06 +0100

    small refactoring

--- source/2016/04/solution.cpp
+++ source/2016/04/solution.cpp
@@ -5,11 +5,6 @@ template <>
 auto advent2016::day04() -> result {
     auto input = aoc::util::readlines("./source/2016/04/input.txt");
 
-    constexpr auto nletters { 26 };
-    std::array<char, nletters> letters{};
-    std::iota(letters.begin(), letters.end(), 'a');
-    std::array<u32, nletters> counts{};
-
     auto part1{0};
     auto part2{0};
     for (auto const& s : input) {
@@ -19,12 +14,15 @@ auto advent2016::day04() -> result {
         std::string_view id { &s[a + 1], b - a - 1 };
         std::string_view code { &s[b + 1], s.size() - b - 2 };
 
+        auto let = aoc::constants::alphabet;
+        std::array<u32, let.size()> counts{};
         std::fill(counts.begin(), counts.end(), 0);
         for (auto c : lz::filter(room, std::not_fn(aoc::equals<'-'>))) {
-            ++counts[c - 'a'];
+            ++counts[c-'a'];
         }
-        auto let = letters;
-        std::ranges::sort(let, [&](auto lhs, auto rhs) { return std::tie(counts[rhs - 'a'], lhs) < std::tie(counts[lhs - 'a'], rhs); });
+        std::ranges::sort(let, [&](auto lhs, auto rhs) {
+            return std::tie(counts[rhs-'a'], lhs) < std::tie(counts[lhs-'a'], rhs);
+        });
         auto const zipped = lz::zip(std::span{let.data(), code.size()}, code);
         auto id_value = scn::scan_value<int>(id).value();
 
@@ -34,7 +32,7 @@ auto advent2016::day04() -> result {
             std::string name;
             for (auto c : room) {
                 if (c == '-') { break; }
-                name.push_back(letters[(c - 'a' + id_value) % nletters]);
+                name.push_back(aoc::constants::alphabet[(c - 'a' + id_value) % let.size()]);
             }
             if (name == "northpole") {
                 part2 = id_value;
