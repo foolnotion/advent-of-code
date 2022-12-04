fa2c80b752f75a42e09f725fdc3ef4980ca89aa0
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-12-04 13:11:46 +0100

    2022 day 04

---
+++ source/2022/04/solution.cpp
@@ -0,0 +1,32 @@
+#include <aoc.hpp>
+
+namespace detail {
+    using point = aoc::point<i32, 2>;
+
+    auto const parse = [](auto const& line) -> std::pair<point, point> {
+        point a;
+        point b;
+        (void)scn::scan(line, "{}-{},{}-{}", a[0], a[1], b[0], b[1]);
+        return {a,b};
+    };
+
+    template<bool B = true>
+    auto contains(std::pair<point, point> p) -> bool {
+        auto [a, b] = p;
+        return (a[0] <= b[0] && a[1] >= b[1]) || (B && contains<!B>({b, a}));
+    };
+
+    template<bool B = true>
+    auto overlaps(std::pair<point, point> p) -> bool {
+        auto [a, b] = p;
+        return (a[0] <= b[0] && a[1] >= b[0]) || (B && overlaps<!B>({b, a}));
+    };
+} // namespace detail
+
+template<>
+auto advent2022::day04() -> result {
+    auto pairs = lz::map(aoc::util::readlines("./source/2022/04/input.txt"), detail::parse).toVector();
+    auto part1 = std::transform_reduce(pairs.begin(), pairs.end(), u64{0}, std::plus{}, detail::contains<true>);
+    auto part2 = std::transform_reduce(pairs.begin(), pairs.end(), u64{0}, std::plus{}, detail::overlaps<true>);
+    return aoc::result(part1, part2);
+}
