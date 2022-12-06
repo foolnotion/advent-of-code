889f1ebe4d99e941697324c3bf9d1157611bc814
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-12-06 01:21:45 +0100

    2016 day 17

--- source/2016/17/solution.cpp
+++ source/2016/17/solution.cpp
@@ -1,6 +1,57 @@
 #include <aoc.hpp>
+#include <util/md5.h>
+
+namespace detail {
+    using point = Eigen::Array<i32, 2, 1>;
+
+    struct state {
+        static constexpr auto passcode{"dmypynyp"};
+        static constexpr auto doors{"UDLR"};
+
+        std::string path{};
+        point pos{0,0};
+
+        [[nodiscard]] static inline auto is_open(char c) { return aoc::equals<'b', 'c', 'd', 'e', 'f'>(c); }
+        [[nodiscard]] inline auto neighbors() const {
+            std::vector<state> nb;
+            auto s = MD5{}(fmt::format("{}{}", passcode, path));
+
+            std::array<point, 4> moves{ point{-1,  0}, {+1,  0}, { 0, -1}, { 0, +1} };
+            for (auto i = 0; i < 4; ++i) {
+                if (!is_open(s[i])) { continue; }
+                auto newpos = pos+moves[i];
+                if ((newpos >= 0 && newpos <= 3).all()) {
+                    state q{path, newpos};
+                    q.path.push_back(doors[i]); // NOLINT
+                    nb.push_back(q);
+                }
+            }
+            return nb;
+        }
+    };
+} // namespace detail
 
 template<>
 auto advent2016::day17() -> result {
-    return aoc::result("", "");
+    std::queue<detail::state> queue;
+    queue.push({"", {0,0}});
+    std::string shortest_path;
+    auto longest_path{0UL};
+    detail::point goal{3,3};
+
+    while(!queue.empty()) {
+        auto s = queue.front();
+        queue.pop();
+
+        if ((s.pos == goal).all()) {
+            if (shortest_path.empty()) { shortest_path = s.path; }
+            longest_path = std::max(longest_path, s.path.size());
+            continue;
+        }
+
+        for (auto&& q : s.neighbors()) {
+            queue.push(q);
+        }
+    }
+    return aoc::result(shortest_path, longest_path);
 }
