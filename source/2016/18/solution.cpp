fe7a9f2e97f56eff188ee8ba3ed34e19afa1e2e9
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-12-06 01:21:50 +0100

    2016 day 18

--- source/2016/18/solution.cpp
+++ source/2016/18/solution.cpp
@@ -2,5 +2,26 @@
 
 template<>
 auto advent2016::day18() -> result {
-    return aoc::result("", "");
+    std::string input{".^^^^^.^^.^^^.^...^..^^.^.^..^^^^^^^^^^..^...^^.^..^^^^..^^^^...^.^.^^^^^^^^....^..^^^^^^.^^^.^^^.^^"};
+    constexpr auto rows_p1{40};
+    constexpr auto rows_p2{400'000};
+
+    auto count = [&](auto rows) {
+        std::vector<u8> v{1};
+        std::transform(input.begin(), input.end(), std::back_inserter(v), aoc::equals<'.'>);
+        v.push_back(1);
+        auto u = v;
+
+        auto sum = std::reduce(v.begin(), v.end(), 0UL);
+        for (auto i = 0; i < rows-1; ++i) {
+            for (auto j = 1; j < std::ssize(v)-1; ++j) {
+                u[j] = v[j-1] == v[j+1];
+            }
+            std::swap(u, v);
+            sum += std::reduce(v.begin(), v.end());
+        }
+        return sum - 2 * rows;
+    };
+
+    return aoc::result(count(rows_p1), count(rows_p2));
 }
