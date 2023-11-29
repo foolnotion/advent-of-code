d41bce5481f4dc2124dc61b5c2d56a4b868bee9c
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-11-29 15:55:26 +0100

    2017 day 13

--- source/2017/13/solution.cpp
+++ source/2017/13/solution.cpp
@@ -1,6 +1,29 @@
 #include <aoc.hpp>
+#include <bits/ranges_util.h>
 
 template<>
 auto advent2017::day13() -> result {
-    return aoc::result("", "");
+    using layer = std::pair<i32, i32>;
+    auto input = aoc::util::readlines("./source/2017/13/input.txt");
+    std::vector<layer> layers;
+    layers.reserve(input.size());
+    for (auto const& s : input) {
+        layer l{};
+        (void)scn::scan(s, "{}: {}", l.first, l.second);
+        layers.push_back(l);
+    }
+    auto scanner_top = [](layer l) {
+        return l.first % (2 * (l.second - 1)) == 0;
+    };
+
+    // part 1 - compute severity
+    auto const p1 = std::transform_reduce(layers.begin(), layers.end(), 0, std::plus{}, [&](auto l) {
+        return scanner_top(l) ? l.first * l.second : 0;
+    });
+
+    // part 2 - compute minimum delay
+    auto const p2 = *std::ranges::find_if(std::views::iota(1), [&](auto i) {
+        return std::ranges::none_of(layers, [&](auto l) { return scanner_top({l.first+i, l.second}); });
+    });
+    return aoc::result(p1, p2);
 }
