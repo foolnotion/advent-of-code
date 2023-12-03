8ff6b5a2a8d8dc7d913c075c8442bd095a35a8c5
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-12-03 01:35:45 +0100

    2017 day 11 fix compile in debug

--- source/2017/11/solution.cpp
+++ source/2017/11/solution.cpp
@@ -5,12 +5,12 @@ template<>
 auto advent2017::day11() -> result {
     using point = Fastor::Tensor<i32, 3>;
 
-    static constexpr point n  = point{+1, -1, 0};
-    static constexpr point nw = point{0, -1, +1};
-    static constexpr point sw = point{-1, 0, +1};
-    static constexpr point s  = point{-1, +1, 0};
-    static constexpr point se = point{0, +1, -1};
-    static constexpr point ne = point{+1, 0, -1};
+    point const n  = point{+1, -1, 0};
+    point const nw = point{0, -1, +1};
+    point const sw = point{-1, 0, +1};
+    point const s  = point{-1, +1, 0};
+    point const se = point{0, +1, -1};
+    point const ne = point{+1, 0, -1};
 
     aoc::dense::map<std::string_view, point> move {
         { "n" , n  },
@@ -21,7 +21,7 @@ auto advent2017::day11() -> result {
         { "ne", ne }
     };
 
-    auto input = aoc::util::readlines("./source/2017/11/input.txt").front(); 
+    auto input = aoc::util::readlines("./source/2017/11/input.txt").front();
     auto distance = [](auto const p) { return Fastor::max(Fastor::abs(p)); };
 
     point p{0,0,0};
