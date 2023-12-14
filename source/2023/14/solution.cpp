4e98a197ebf739c32f73eb691cd32f5dc375cca2
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-12-15 00:30:22 +0100

    2023 day 14 optimizations

--- source/2023/14/solution.cpp
+++ source/2023/14/solution.cpp
@@ -13,16 +13,11 @@ namespace {
     }
 
     auto tilt(auto& m) {
-        bool moved{true};
-        while(moved) {
-            moved = false;
-            for (auto j = 0; j < m.cols(); ++j) {
-                for (auto i = 1; i < m.rows(); ++i) {
-                    if (m(i, j) != 'O') { continue; }
-                    for (auto k = i; k > 0 && m(k-1, j) == '.'; --k) {
-                        moved = true;
-                        std::swap(m(k, j), m(k-1, j));
-                    }
+        for (auto j = 0; j < m.cols(); ++j) {
+            for (auto i = 1; i < m.rows(); ++i) {
+                if (m(i, j) != 'O') { continue; }
+                for (auto k = i; k > 0 && m(k-1, j) == '.'; --k) {
+                    std::swap(m(k, j), m(k-1, j));
                 }
             }
         }
@@ -39,6 +34,19 @@ namespace {
         auto f = [&](auto i) { return (m.rows()-i) * (m.row(i) == 'O').count(); };
         return fp::transform_reduce(f, std::plus{}, 0L, fp::numbers(0L, m.rows()));
     };
+
+    auto spin_cycle(auto m, auto n) {
+        aoc::dense::map<u64, std::pair<i64, i64>> seen;
+        aoc::util::hash hash;
+        for (auto i = 0L; i < n; ++i) {
+            if (auto [it, ok] = seen.insert({ hash(m.data(), m.size()), { i, load(m) } }); !ok) {
+                auto [j, _] = it->second;
+                return seen.values()[j + (n-j) % (i-j)].second.second;
+            }
+            spin(m);
+        }
+        return load(m);
+    }
 } // namespace
 
 template<>
@@ -46,24 +54,10 @@ auto advent2023::day14() -> result {
     auto input = aoc::util::readlines("./source/2023/14/input.txt");
     auto m = parse(input);
 
-    // part 1
     tilt(m);
     auto const p1 = load(m);
 
-    // part 2
-    constexpr auto n{1'000'000'000}; // total cycles
-    aoc::dense::map<u64, i64> seen;
-    aoc::util::hash hash;
-    bool found_cycle{false};
-    for (auto i = 0L; i < n; ++i) {
-        spin(m);
-        if (found_cycle) { continue; }
-        if (auto [it, ok] = seen.insert({ hash(m.reshaped()), i }); !ok) {
-            found_cycle = true;
-            auto const k{it->second};
-            i = n - (n-k) % (i-k);
-        }
-    }
-    auto const p2 = load(m);
+    constexpr auto n{1'000'000'000};
+    auto const p2 = spin_cycle(m, n);
     return aoc::result(p1, p2);
 }
