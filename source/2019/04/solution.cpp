b573fcd9ad20e9a3d68ea8a7fdf9a25b7037eb6d
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-12-18 21:31:54 +0100

    2019 day 04 simplify code

--- source/2019/04/solution.cpp
+++ source/2019/04/solution.cpp
@@ -18,11 +18,11 @@ auto advent2019::day04() -> result {
         for (auto i = 0; i < x.size()-1; ++i) {
             auto j = i;
             while(j < x.size() && x[j] == x[i]) { ++j; }
-            if (p2 && j-i == 2) { return true; }
-            if (!p2 && j-i >= 2) { return true; }
+            if (p2 && j-i == 2) { return 1L; }
+            if (!p2 && j-i >= 2) { return 1L; }
             i = j-1;
         }
-        return false;
+        return 0L;
     };
 
     auto a = get_digits(range.first);
@@ -36,21 +36,13 @@ auto advent2019::day04() -> result {
         return true;
     };
 
-    auto f = [&](auto x, auto i, auto p2, auto&& f) {
+    auto f = [&](auto x, auto i, auto p2, auto&& f) -> i64 {
         auto c{0};
-        if (i == size-1) {
-            for (auto k = x[i-1]; k < base; ++k) {
-                x[i] = k;
-                if (!compare(x, b)) { break; }
-                c += has_double_digit(x, p2);
-            }
-            return c;
-        }
         auto m = i == 0 ? a[i] : x[i-1];
         for (auto k = m; k < base; ++k) {
             x[i] = k;
             if (!compare(x, b)) { break; }
-            c += f(x, i+1, p2, f);
+            c += i == size-1 ? has_double_digit(x, p2) : f(x, i+1, p2, f);
         }
         return c;
     };
