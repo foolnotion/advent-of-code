fe6ddbcd8aab074853c0c53b60ed702d0a667b8f
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-12-28 01:04:34 +0100

    2023 day 21 optimized solution

--- source/2023/21/solution.cpp
+++ source/2023/21/solution.cpp
@@ -1,124 +1,95 @@
 #include <aoc.hpp>
-#include <experimental/mdspan>
+#include <util/dynamic_bitset.hpp>
 #include <Eigen/Dense>
 
-namespace rs = std::ranges;
-
-using point   = aoc::point<i32, 2>;
-using extents = std::experimental::extents<i32, std::dynamic_extent, std::dynamic_extent>;
-using mdspan  = std::experimental::mdspan<char, extents>;
-
 namespace {
+    using point = Eigen::Vector2i;
+    using vec3d = Eigen::Vector3d;
+
+    struct grid {
+        Eigen::Array<char, -1, -1> m;
+
+        [[nodiscard]] inline auto remap(auto i, auto j) const {
+            return std::tuple{aoc::math::mod_euclid(i, m.rows()), aoc::math::mod_euclid(j, m.cols())};
+        }
+
+        auto operator()(i32 i, i32 j) const -> char {
+            auto [x, y] = remap(i, j);
+            return m(x, y);
+        }
+
+        template<std::size_t N = 1>
+        [[nodiscard]] auto reachable(std::array<i32, N> steps) const -> std::array<i32, N> {
+            point s{m.rows()/2, m.cols()/2};
+            std::deque<std::pair<point, i32>> queue{{s, 0}};
+
+            auto neighbors = [&](point p) {
+                return std::array<point, 4> {
+                    point{ p[0]-1, p[1] }, { p[0]+1, p[1] }, { p[0], p[1]-1 }, { p[0], p[1]+1 }
+                };
+            };
+
+            auto parity = [](auto x) { return x % 2; };
+            std::array<i32, N> total{};
+
+            auto const n{steps.back()};
+            auto const d{2UL*n+1UL};
+            sul::dynamic_bitset<u64> seen(d*d);
+
+            while(!queue.empty()) {
+                auto [p, t] = queue.front();
+                queue.pop_front();
+
+                auto k{(p(0)-s(0)+n) * d + (p(1)-s(1)+n)};
+                if (seen.test_set(k)) { continue; }
+
+                // total += t <= n && parity(t) == parity(n);
+                for (auto i = 0; i < N; ++i) {
+                    total[i] += t <= steps[i] && parity(t) == parity(steps[i]);
+                }
+                if (t+1 > n) { continue; }
+
+                for (auto q : neighbors(p)) {
+                    auto [x, y] = remap(q(0), q(1));
+                    if (m(x, y) != '#') { queue.emplace_back(q, t+1); }
+                }
+            }
+            return total;
+        }
+    };
+
     auto parse(auto const& input) {
-        std::vector<char> buf;
-        buf.reserve(input.size()* input.front().size());
-        auto copy = [&](auto const& s) { rs::copy(s, std::back_inserter(buf)); };
-        rs::for_each(input, copy);
-        return buf;
+        Eigen::Array<char, -1, -1> m(input.size(), input.front().size());
+        for (auto i = 0; i < input.size(); ++i) {
+            std::ranges::copy(input[i], m.row(i).begin());
+        }
+        return grid{m};
+    }
+
+    auto find_roots(Eigen::Vector3d xs, Eigen::Vector3d ys) -> Eigen::Vector3d {
+        Eigen::Matrix3d vm;
+        vm.col(0).setConstant(1);
+        vm.col(1) = xs;
+        vm.col(2) = xs.array().square();
+        return vm.householderQr().solve(ys);
     }
 } // namespace
 
 template<>
 auto advent2023::day21() -> result {
     auto input = aoc::util::readlines("./source/2023/21/input.txt");
-    auto const nrows{input.size()};
-    auto const ncols{input.front().size()};
-    auto buf = parse(input);
-    mdspan grid(buf.data(), nrows, ncols);
+    auto grid = parse(input);
 
-    auto s = rs::find(buf, 'S');
-    auto x = std::distance(buf.begin(), s) / ncols;
-    auto y = std::distance(buf.begin(), s) % ncols;
-    point p{static_cast<i32>(x), static_cast<i32>(y)};
-
-    auto remap = [&](point p) -> point {
-        auto mod = [](auto a, auto b) -> i32 {
-            while(a < 0) { a += b; }
-            return a % b;
-        };
-        return point{mod(p[0], nrows), mod(p[1], ncols)};
-    };
-
-    auto valid = [&](point p) -> bool {
-        auto [x, y] = remap(p);
-        return x >= 0 && x < nrows && y >= 0 && y < ncols && aoc::equals<'.', 'S'>(grid(x, y));
-    };
-
-    auto neighbors = [&](point p) {
-        return std::array {
-            point { p[0]-1, p[1] },
-            point { p[0]+1, p[1] },
-            point { p[0], p[1]-1 },
-            point { p[0], p[1]+1 }
-        };
-    };
-
-    auto hash = [](auto p) {
-        // std::array a{p.first, p.second[0], p.second[1]};
-        // return aoc::util::hash{}(a);
-        auto [c, q] = p;
-        return aoc::util::hash{}(c, q[0], q[1]);
-    };
-
-    std::vector<i64> dist(nrows * ncols, 0L);
-    std::experimental::mdspan<i64, extents> distance(dist.data(), nrows, ncols);
-
-    // part 1
-    constexpr auto steps{64};
-    auto bfs = [&](auto n) {
-        auto count{0UL};
-        aoc::dense::set<std::pair<i32, point>, decltype(hash)> seen;
-        std::deque<std::pair<i64, point>> queue;
-        queue.emplace_back(0, p);
-
-        auto hits{0UL};
-        while(!queue.empty()) {
-            auto [c, q] = queue.front();
-            queue.pop_front();
-
-            if (c > n) { continue; }
-
-            auto r = remap(q);
-            // auto& d = distance()
-            if (auto [it, ok] = seen.insert({c, q}); !ok) {
-                ++hits;
-                continue;
-            }
-
-            count += c == n;
-
-            if (c < n) {
-                for (auto r : neighbors(q) | std::views::filter(valid)) {
-                    queue.emplace_back(c+1, r);
-                }
-            }
-        }
-        fmt::print("seen: {}, hits: {}\n", seen.size(), hits);
-        return count;
-    };
-    auto const p1 = bfs(steps);
+    constexpr auto steps_p1{64L};
+    auto const p1 = grid.reachable({steps_p1})[0];
 
     // part 2
     constexpr auto steps_p2{26501365L};
-    constexpr auto target{(steps_p2 - 65) / 131};
-    Eigen::Vector3d xs({0, 1, 2});
-    Eigen::Vector3d ys(3);
-
-    // steps ncols/2, ncols/2+ncols, ncols/2+2*ncols
-    rs::transform(std::array{65, 196, 327}, ys.data(), bfs);
-
-    // construct vandermonde matrix
-    Eigen::Matrix3d vm;
-    vm.col(0).setConstant(1);
-    vm.col(1) = xs;
-    vm.col(2) = xs.array().square();
-    Eigen::Vector3d roots = vm.householderQr().solve(ys);
-
-    auto f = [&](auto i) -> i64 {
-        return static_cast<i64>(std::round(roots(i))) * std::pow(target, i);
-    };
-    constexpr std::array idx{0, 1, 2};
-    auto const p2 = std::transform_reduce(idx.begin(), idx.end(), 0UL, std::plus{}, f);
-
+    constexpr auto t{(steps_p2 - 65L) / 131L};
+    vec3d x{0, 1, 2};
+    auto r{grid.reachable<3>({65L, 196L, 327L})};
+    vec3d y{r[0], r[1], r[2]};
+    vec3d roots = find_roots(x, y).array().round();
+    auto const p2 = (vec3d::Constant(t).array().pow(x.array()) * roots.array()).sum();
     return aoc::result(p1, p2);
 }
