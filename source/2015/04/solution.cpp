0fb4ed7a5938c09fcb79ff2f45fe2ee7faad2e07
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-09-15 22:41:59 +0200

    refactor a couple of 2015 solutions

--- source/2015/04/solution.cpp
+++ source/2015/04/solution.cpp
@@ -6,15 +6,17 @@ auto advent2015::day04() -> result {
     constexpr auto input { "iwrupvqb" };
     std::array<u8, 16> buf{};
 
-    auto count = [&](int n) -> std::optional<i64> {
-        for (auto i = 0;; ++i) {
-            hashing::md5::hash_bs(input + std::to_string(i), buf.data());
-            auto cnt = std::transform_reduce(buf.begin(), buf.begin() + n/2 + n%2, 0UL, std::plus{},
-                    [&](auto c) { return ((c & 0xF0) == 0) + ((c & 0x0F) == 0); }); // NOLINT
+    auto zeros = [&](auto c) { return ((c & 0xF0) == 0) + ((c & 0x0F) == 0); };
 
-            if (cnt >= n) { return std::optional{i}; }
-        }
-        return std::nullopt;
+    auto count = [&](auto i, auto n) {
+        hashing::md5::hash_bs(input + std::to_string(i), buf.data());
+        return std::transform_reduce(buf.begin(), buf.begin() + n/2 + n%2, 0UL, std::plus{}, zeros);
     };
-    return aoc::result(count(5).value(), count(6).value());
+
+    auto find = [&](auto n) {
+        return *std::ranges::find_if(std::views::iota(0), [&](auto i) { return count(i, n) >= n; });
+    };
+
+    constexpr auto n{5};
+    return aoc::result(find(n), find(n+1));
 }
