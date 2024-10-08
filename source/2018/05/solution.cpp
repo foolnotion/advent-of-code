c1ebadd95748c5a9cb79492b669ed597cf46a3fc
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-10-08 07:03:56 +0200

    2018 day 5

--- source/2018/05/solution.cpp
+++ source/2018/05/solution.cpp
@@ -2,10 +2,30 @@
 
 template<>
 auto advent2018::day05() -> result {
-    auto lines = aoc::util::readlines("source/2058/05/input.txt");
-    auto values = lines | std::views::transform(aoc::util::read<i32>);
+    auto lines = aoc::util::readlines("source/2018/05/input.txt");
+    auto input = lines.front();
 
-    auto const p1 = std::reduce(values.begin(), values.end());
+    auto opposites = [](char a, char b) {
+        return a != b && std::tolower(a) == std::tolower(b);
+    };
 
-    return aoc::result(p1, "");
+    auto react = [&](std::string const& polymer) {
+        std::string left;
+        for (auto c : polymer) {
+            if (!left.empty() && opposites(left.back(), c)) { left.pop_back(); }
+            else                                            { left += c; }
+        }
+        return left.size();
+    };
+
+    auto remove_reactant = [](std::string polymer, char c) {
+        std::erase_if(polymer, [&](auto x) { return std::tolower(x) == c; });
+        return polymer;
+    };
+
+    auto const p1 = react(input);
+    auto const p2 = std::ranges::min(aoc::constants::alphabet | std::views::transform([&](auto c) {
+        return react(remove_reactant(input, c));
+    }));
+    return aoc::result(p1, p2);
 }
